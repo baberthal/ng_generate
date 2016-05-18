899770ef62c0b8313b8ef4ab6bbd80df29124583
Morgan Lieberthal <morgan@jmorgan.org>
2016-05-17 19:56:05 -0600

    Add configurations

--- lib/ng/generators.rb
+++ lib/ng/generators.rb
@@ -1,1 +1,17 @@
 require 'thor/group'
+
+require 'active_support'
+require 'active_support/core_ext/object/blank'
+require 'active_support/core_ext/array/extract_options'
+require 'active_support/core_ext/hash/deep_merge'
+require 'active_support/core_ext/module/attribute_accessors'
+require 'active_support/core_ext/string/inflections'
+
+module Ng
+  module Generators
+    autoload :Actions, 'ng/generators/actions'
+    autoload :Base, 'ng/generators/base'
+
+    mattr_accessor :namespace
+  end
+end
--- ng_generate.gemspec
+++ ng_generate.gemspec
@@ -23,4 +23,5 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency 'rspec', '~> 3.0'
 
   spec.add_runtime_dependency 'thor', '~> 0.19'
+  spec.add_runtime_dependency 'activesupport', '~> 4.2'
 end
